f74379139406ce803df0c68565270a6a08df18c8
Marin Usalj <mneorr@gmail.com>
2013-05-06 19:15:29 +0200

    inlined pragma marks

--- Classes/KWBeNilMatcher.m
+++ Classes/KWBeNilMatcher.m
@@ -11,22 +11,19 @@
 
 @implementation KWBeNilMatcher
 
-#pragma mark -
-#pragma mark Getting Matcher Strings
+#pragma mark - Getting Matcher Strings
 
 + (NSArray *)matcherStrings {
   return @[@"beNil"];
 }
 
-#pragma mark -
-#pragma mark Matching
+#pragma mark - Matching
 
 - (BOOL)evaluate {
   return (BOOL)(self.subject == nil);
 }
 
-#pragma mark -
-#pragma mark Getting Failure Messages
+#pragma mark - Getting Failure Messages
 
 - (NSString *)failureMessageForShould {
   return [NSString stringWithFormat:@"expected subject to be nil, got %@",
